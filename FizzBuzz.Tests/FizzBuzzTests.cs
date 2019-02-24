a6efb24000711bd377a08589c4981897d66c9827
StojanovicMilos <s.milos@sbb.rs>
2019-02-24 10:48:29 +0100

    Solve FizzBuzz problem

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(33)]
+        [InlineData(45)]
+        [InlineData(99)]
+        public void DividableByThreeContainsFizz(int numberDividableByThree)
+        {
+            //arrange
+            string expectedSubstring = "Fizz";
+
+            //act
+            string output = Program.FizzBuzz(numberDividableByThree);
+
+            //assert
+            Assert.Contains(expectedSubstring, output);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(20)]
+        [InlineData(45)]
+        [InlineData(100)]
+        public void DividableByFiveContainsBuzz(int numberDividableByFive)
+        {
+            //arrange
+            string expectedSubstring = "Buzz";
+
+            //act
+            string output = Program.FizzBuzz(numberDividableByFive);
+
+            //assert
+            Assert.Contains(expectedSubstring, output);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(30)]
+        [InlineData(45)]
+        [InlineData(60)]
+        [InlineData(75)]
+        [InlineData(90)]
+        public void DividableByFifteenContainsBuzz(int numberDividableByFifteen)
+        {
+            //arrange
+            string expectedOutput = "FizzBuzz";
+
+            //act
+            string output = Program.FizzBuzz(numberDividableByFifteen);
+
+            //assert
+            Assert.Equal(expectedOutput, output);
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(8)]
+        [InlineData(46)]
+        [InlineData(98)]
+        public void NotDividableByThreeOrFiveContainsNumber(int numberNotDividableByThreeOrFive)
+        {
+            //arrange
+            string expectedoutput = numberNotDividableByThreeOrFive.ToString();
+
+            //act
+            string output = Program.FizzBuzz(numberNotDividableByThreeOrFive);
+
+            //assert
+            Assert.Equal(expectedoutput, output);
+        }
+
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace FizzBuzz
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+﻿using System;
 
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main()
+        {
+            RunFizzBuzz();
+            Console.ReadLine();
+        }
+
+        private static void RunFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(FizzBuzz(i));
+            }
+        }
+
+        public static string FizzBuzz(int i)
+        {
+            string output = string.Empty;
+            if (i % 3 == 0)
+            {
+                output += "Fizz";
+            }
+
+            if (i % 5 == 0)
+            {
+                output += "Buzz";
+            }
+
+            if (output == string.Empty)
+            {
+                output = i.ToString();
+            }
+
+            return output;
         }
     }
 }
