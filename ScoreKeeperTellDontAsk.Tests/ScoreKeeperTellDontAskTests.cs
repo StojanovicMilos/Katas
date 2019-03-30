342b47f5c13540f074d5074653a15d41be050f87
Milos Stojanovic <s.milos@sbb.rs>
2019-03-30 13:40:23 +0100

    solve scorekeeper kata with tell don't ask constraint

---
+++ ScoreKeeperTellDontAsk.Tests/ScoreKeeperTellDontAskTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ScoreKeeperTellDontAsk.Score;
+using ScoreKeeperTellDontAsk.Scoreboard;
+using ScoreKeeperTellDontAsk.ScoreKeeper;
+using Xunit;
+
+namespace ScoreKeeperTellDontAsk.Tests
+{
+    public class ScoreKeeperTellDontAskTests
+    {
+        [Fact]
+        public void ScoreKeeperDisplaysRightResult()
+        {
+            //arrange
+            const int initialScore = 0;
+            IScoreboard scoreboard = new TestingScoreboard();
+            ITeamScore homeTeamScore = new IntTeamScore(initialScore, scoreboard);
+            ITeamScore awayTeamScore = new IntTeamScore(initialScore, scoreboard);
+            IScoreKeeper scoreKeeper = new DefaultScoreKeeper(homeTeamScore, awayTeamScore, scoreboard);
+            string expectedResult = "003:000" + Environment.NewLine;
+
+            //act
+            scoreKeeper.ScoreTeamA3();
+
+            //assert
+            Assert.Equal(expectedResult, ((TestingScoreboard) scoreboard).DisplayedText);
+        }
+    }
+}
---
+++ ScoreKeeperTellDontAsk/ScoreKeeper/ScoreKeeper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using ScoreKeeperTellDontAsk.Score;
+using ScoreKeeperTellDontAsk.Scoreboard;
+
+namespace ScoreKeeperTellDontAsk.ScoreKeeper
+{
+    public class DefaultScoreKeeper : IScoreKeeper
+    {
+        private readonly ITeamScore _homeTeamScore;
+        private readonly ITeamScore _awayTeamScore;
+
+        private readonly IScoreboard _scoreboard;
+
+        public DefaultScoreKeeper(ITeamScore homeScore, ITeamScore awayScore, IScoreboard scoreboard)
+        {
+            _homeTeamScore = homeScore;
+            _awayTeamScore = awayScore;
+            _scoreboard = scoreboard;
+        }
+
+        public void ScoreTeamA1()
+        {
+            _homeTeamScore.Score1();
+            DisplayScore();
+        }
+
+        public void ScoreTeamA2()
+        {
+            _homeTeamScore.Score2();
+            DisplayScore();
+        }
+
+        public void ScoreTeamA3()
+        {
+            _homeTeamScore.Score3();
+            DisplayScore();
+        }
+
+        public void ScoreTeamB1()
+        {
+            _awayTeamScore.Score1();
+            DisplayScore();
+        }
+
+        public void ScoreTeamB2()
+        {
+            _awayTeamScore.Score2();
+            DisplayScore();
+        }
+
+        public void ScoreTeamB3()
+        {
+            _awayTeamScore.Score3();
+            DisplayScore();
+        }
+
+        private void DisplayScore()
+        {
+            _homeTeamScore.Display();
+            _scoreboard.Display(":");
+            _awayTeamScore.Display();
+            _scoreboard.Display(Environment.NewLine);
+        }
+    }
+}
