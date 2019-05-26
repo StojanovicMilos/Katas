963b463e4871a29cd5821180ac124c0aee7dfddb
StojanovicMilos <s.milos@sbb.rs>
2019-05-26 09:12:59 +0200

    solve isomorphic strings problem

---
+++ IsomorphicStrings.Tests/IsomorphicStringsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace IsomorphicStrings.Tests
+{
+    public class IsomorphicStringsTests
+    {
+        public static IEnumerable<object[]> IsomorphicStringsTestData
+        {
+            get
+            {
+                var list = new List<object[]>
+                {
+                    new object[] {"brain", "space", true},
+                    new object[] {"noon", "feet", false},
+                    new object[] {"aab", "ccd", true},
+                };
+                foreach (var element in list)
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(IsomorphicStringsTestData))]
+        public void TestIsomorphicStrings(string a, string b, bool expectedResult)
+        {
+            //arrange
+
+            //act
+            bool actualResult = IsomorphicStringsChecker.AreStringsIsomorphic(a, b);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+    }
+}
---
+++ IsomorphicStrings/IsomorphicStringsChecker.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace IsomorphicStrings
+{
+    public static class IsomorphicStringsChecker
+    {
+        /*
+         Two strings A and B are isomorphic if there exists character mapping which maps characters from A to get B. Rules for character mapping are:
+             - all occurrences of one character must be replaced with the same character
+             - no two different characters may map to the same character
+             - a character may map to itself
+         
+          Write a function that returns true if provided strings are isomorphic, otherwise false
+         */
+
+        public static bool AreStringsIsomorphic(string a, string b)
+        {
+            if (a.Length != b.Length)
+                return false;
+
+            Dictionary<char, char> mapping = new Dictionary<char, char>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char key = a[i];
+                char value = b[i];
+
+                if (mapping.ContainsKey(key))
+                {
+                    mapping.TryGetValue(key, out var existingValue);
+                    if (existingValue != value)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    mapping.Add(key, value);
+                }
+            }
+
+            return true;
+        }
+    }
+}
