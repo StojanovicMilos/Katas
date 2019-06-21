6db937adac85a3fb87d434e5b99b7808a8a2d37f
StojanovicMilos <s.milos@sbb.rs>
2019-06-21 16:53:31 +0200

    Solve converting array to tree problem

---
+++ ConvertingArrayToTree.Tests/PreOrderTraversalTests.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ConvertingArrayToTree.Tests
+{
+    public class PreOrderTraversalTests
+    {
+        public static IEnumerable<object[]> PreOrderTraversalTestData
+        {
+            get
+            {
+                var list = new List<object[]>
+                {
+                    new object[]
+                    {
+                        new TreeNode
+                        {
+                            Value = 3,
+                            FirstChild = new TreeNode
+                            {
+                                Value = 1,
+                                FirstChild = new TreeNode
+                                {
+                                    Value = 4,
+                                    Sibling = new TreeNode
+                                    {
+                                        Value = 48
+                                    }
+                                },
+                                Sibling = new TreeNode
+                                {
+                                    Value = 2,
+                                    Sibling = new TreeNode
+                                    {
+                                        Value = 5,
+                                        FirstChild = new TreeNode
+                                        {
+                                            Value = 3,
+                                            FirstChild = new TreeNode
+                                            {
+                                                Value = 6,
+                                                Sibling = new TreeNode()
+                                                {
+                                                    Value = 18
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                        },
+                        new[] {3, 1, 4, 48, 2, 5, 3, 6, 18}
+                    },
+
+                    new object[]
+                    {
+                        new TreeNode
+                        {
+                            Value = 6,
+                            FirstChild = new TreeNode
+                            {
+                                Value = 2,
+                                FirstChild = new TreeNode
+                                {
+                                    Value = 1,
+                                    Sibling = new TreeNode
+                                    {
+                                        Value = 4,
+                                        FirstChild = new TreeNode
+                                        {
+                                            Value = 3,
+                                            Sibling = new TreeNode
+                                            {
+                                                Value = 5
+                                            }
+                                        }
+                                    }
+                                },
+                                Sibling = new TreeNode
+                                {
+                                    Value = 7,
+                                    FirstChild = new TreeNode
+                                    {
+                                        Value = 9,
+                                        FirstChild = new TreeNode
+                                            {Value = 8}
+
+                                    }
+                                }
+                            }
+                        },
+                        new[] {6, 2, 1, 4, 3, 5, 7, 9, 8}
+                    }
+                };
+                foreach (var element in list)
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(PreOrderTraversalTestData))]
+        public void PreOrderTraversalTest(TreeNode head, int[] expectedResult)
+        {
+            //arrange
+
+            //act
+            int[] actualResult = head.PreOrderTraversal();
+
+            //assert
+            for (int i = 0; i < expectedResult.Length; i++)
+            {
+                Assert.Equal(expectedResult[i], actualResult[i]);
+            }
+        }
+    }
+}
---
+++ ConvertingArrayToTree/TreeNodeExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace ConvertingArrayToTree
+{
+    public static class TreeNodeExtensions
+    {
+        public static int[] PreOrderTraversal(this TreeNode current) => current.ListPreOrderTraversal().ToArray();
+
+        private static List<int> ListPreOrderTraversal(this TreeNode current)
+        {
+            List<int> result = new List<int>
+            {
+                current.Value
+            };
+
+            if (current.FirstChild != null)
+            {
+                result.AddRange(current.FirstChild.ListPreOrderTraversal());
+            }
+
+            if (current.Sibling != null)
+            {
+                result.AddRange(current.Sibling.ListPreOrderTraversal());
+            }
+
+            return result;
+        }
+
+        public static bool TreeSatisfiesRequirements(this TreeNode current)
+        {
+            bool evenNodeIsLeaf = current.Value % 2 == 0 && current.FirstChild == null;
+            bool oddNodeIsInnerNode = current.Value % 2 != 0 && current.FirstChild != null;
+
+            if (evenNodeIsLeaf || oddNodeIsInnerNode)
+            {
+
+                bool returnValue = true;
+                if (current.FirstChild != null)
+                    returnValue &= current.FirstChild.TreeSatisfiesRequirements();
+                if (current.Sibling != null)
+                    returnValue &= current.Sibling.TreeSatisfiesRequirements();
+
+                return returnValue;
+            }
+
+            return false;
+        }
+    }
+}
