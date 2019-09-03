dbedf1140fc85fe28dd9d6b855e70e8d047fc733
Milos Stojanovic <s.milos@sbb.rs>
2019-09-03 19:42:36 +0200

    Solve Gilded Rose

---
+++ GildedRose.Tests/GildedRoseTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class GildedRoseTests
+    {
+        [Fact]
+        public void XunitWorks()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ApprovalTests()
+        {
+            IEnumerable<string> names = new[]
+            {
+                "other",
+                "Aged Brie",
+                "Backstage passes to a TAFKAL80ETC concert",
+                "Sulfuras, Hand of Ragnaros"
+            };
+            IEnumerable<int> sellIns = Enumerable.Range(-5, 30);
+            IEnumerable<int> qualities = Enumerable.Range(-5, 60);
+
+            CombinationApprovals.VerifyAllCombinations(
+                (name, sellIn, quality) =>
+                    GildedRose.UpdateQuality(new Item {Name = name, SellIn = sellIn, Quality = quality}),
+                names, sellIns, qualities);
+        }
+    }
+}
---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRose
+{
+    public class GildedRose
+    {
+        private readonly IList<Item> _items;
+
+        public GildedRose(IList<Item> items)
+        {
+            _items = items;
+        }
+
+        public void UpdateQuality()
+        {
+            foreach (var item in _items)
+            {
+                UpdateQuality(item);
+            }
+        }
+
+        public static Item UpdateQuality(Item item)
+        {
+            switch (item.Name)
+            {
+                case "Aged Brie":
+                    return UpdateAgedBrie(item);
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    return UpdateConcert(item);
+                case "Sulfuras, Hand of Ragnaros":
+                    return UpdateSulfuras(item);
+                default:
+                    return DefaultUpdate(item);
+            }
+        }
+
+        private static Item UpdateAgedBrie(Item item)
+        {
+            item.SellIn = item.SellIn - 1;
+            if (item.Quality >= 50)
+                return item;
+            item.Quality = item.Quality + 1;
+            if (item.SellIn >= 0)
+                return item;
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            return item;
+        }
+
+        private static Item UpdateConcert(Item item)
+        {
+            if (item.SellIn < 6 && item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+            if (item.SellIn < 11)
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+                }
+                item.SellIn = item.SellIn - 1;
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+
+                return item;
+            }
+
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            return item;
+        }
+
+        private static Item UpdateSulfuras(Item item)
+        {
+            return item;
+        }
+
+        private static Item DefaultUpdate(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality = item.Quality - 1;
+                }
+            }
+
+            return item;
+        }
+    }
+}
