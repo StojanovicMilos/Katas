5e0d3d9d75be6a5bd2661e907a393aeed4b2ec83
StojanovicMilos <s.milos@sbb.rs>
2019-10-04 13:37:03 +0200

    reorganize time correy challenges to one folder

---
+++ IAmTimCoreyWeekly/TextFileChallenge/ChallengeForm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace TextFileChallenge
+{
+    public partial class ChallengeForm : Form
+    {
+        BindingList<UserModel> users = new BindingList<UserModel>();
+
+        public ChallengeForm()
+        {
+            InitializeComponent();
+
+            WireUpDropDown();
+        }
+
+        private void WireUpDropDown()
+        {
+            usersListBox.DataSource = users;
+            usersListBox.DisplayMember = nameof(UserModel.DisplayText);
+        }
+    }
+}
---
+++ IAmTimCoreyWeekly/TextFileChallenge/UserModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TextFileChallenge
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsAlive { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                string aliveStatus = IsAlive ? "is alive" : "is dead";
+
+                return $"{ FirstName} { LastName } is { Age } and { aliveStatus }";
+            }
+        }
+    }
+}
