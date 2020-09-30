bba4cd45d0542501db376a604a955f006aa50c76
Milos Stojanovic <milos-stojanovic@hotmail.com>
2020-09-30 16:11:09 +0200

    tdd jam session 2 - initial

---
+++ TennisGame.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TennisGame.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void XunitWorks()
+        {
+            Assert.True(true);
+        }
+    }
+}
