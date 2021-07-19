4f969488fc55f0d5f56462952a29a7f5eac3a3e1
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-07-19 18:58:12 +0200

    solve two sum

---
+++ LeetCodeChallenges/TwoSum/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TwoSum
+{
+    //https://leetcode.com/problems/two-sum
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num1 = nums[i];
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    var num2 = nums[j];
+                    if (num1 + num2 == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            throw new Exception();
+        }
+    }
+}
