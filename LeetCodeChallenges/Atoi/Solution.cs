ac4e8fc74338e22f7051c54c94ec70aba5065392
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-07-23 12:52:53 +0200

    solve string to integer atoi

---
+++ LeetCodeChallenges/Atoi/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace Atoi
+{
+    //https://leetcode.com/problems/string-to-integer-atoi
+    public class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            s = s.Trim();
+
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            var sb = new StringBuilder();
+            var i = 0;
+            if (s[i] == '-' || s[i] == '+')
+            {
+                if (s[i] == '-')
+                {
+                    sb.Append(s[i]);
+                }
+                i++;
+            }
+
+            for (; i < s.Length; i++)
+            {
+                if (char.IsNumber(s[i]))
+                {
+                    sb.Append(s[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var numbers = sb.ToString();
+            if (numbers.Length == 0 || numbers == "-" || numbers == "+")
+            {
+                return 0;
+            }
+            int result;
+            try
+            {
+                result = Convert.ToInt32(numbers);
+            }
+            catch
+            {
+                result = numbers[0] == '-' ? int.MinValue : int.MaxValue;
+            }
+
+            return result;
+        }
+    }
+}
