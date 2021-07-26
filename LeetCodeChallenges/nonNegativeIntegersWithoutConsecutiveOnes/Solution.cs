e6c86f64c3b9687d1d9351ce301433fe47308def
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-07-26 12:13:41 +0200

    Solve Non-negative Integers without Consecutive Ones

---
+++ LeetCodeChallenges/nonNegativeIntegersWithoutConsecutiveOnes/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+namespace nonNegativeIntegersWithoutConsecutiveOnes
+{
+    //https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021/611/week-4-july-22nd-july-28th/3826/
+    public class Solution
+    {
+        public int FindIntegersV1(int n) =>
+            Enumerable
+                .Range(0, n + 1)
+                .Count(value =>
+                {
+                    var previous = 0;
+                    while (value > 0)
+                    {
+                        var remainder = value % 2;
+                        if (remainder == 1 && previous == 1)
+                        {
+                            return false;
+                        }
+
+                        previous = remainder;
+                        value /= 2;
+                    }
+
+                    return true;
+                });
+
+        public int FindIntegersV2(int n)
+        {
+            var count = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                if (HasNoConsecutiveOnes(n))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static bool HasNoConsecutiveOnes(in int value)
+        {
+            int i = 31;
+            while (i > 0)
+            {
+                if ((value & (1 << i)) != 0 && (value & (1 << (i - 1))) != 0)
+                    return false;
+                i--;
+            }
+
+            return true;
+        }
+    }
+}
