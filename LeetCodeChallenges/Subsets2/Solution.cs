c569f3b68328ff085bdecfa058872de63b9b3382
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-08-03 17:38:23 +0200

    solve Subsets2

---
+++ LeetCodeChallenges/Subsets2/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Subsets2
+{
+    //https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3837/
+    public class Solution
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            var result = new List<IList<int>> { new List<int>() };
+            foreach (var num in nums.OrderBy(n=>n))
+            {
+                var newSubsets = new List<IList<int>>();
+                foreach (var newSubset in result.Select(oldSubset => oldSubset.ToList()))
+                {
+                    newSubset.Add(num);
+                    newSubsets.Add(newSubset);
+                }
+
+                foreach (var newSubset in newSubsets.Where(newSubset => result.All(list => !list.SequenceEqual(newSubset))))
+                {
+                    result.Add(newSubset);
+                }
+            }
+
+            return result;
+        }
+    }
+}
