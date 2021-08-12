124db634f160e9164b0b042894b482d4ac7f49e0
Milos Stojanovic <milos-stojanovic@hotmail.com>
2021-08-12 18:00:49 +0200

    solve Group Anagrams

---
+++ LeetCodeChallenges/GroupAnagrams/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GroupAnagrams
+{
+    //https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/614/week-2-august-8th-august-14th/3887/
+    public static class Program
+    {
+        public static void Main()
+        {
+            var strs = new[] {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+            IList<IList<string>> result = strs
+                .GroupBy(s => new string(s.OrderBy(c => c).ToArray()))
+                .Select(group =>
+                {
+                    IList<string> list = group.Select(g => g).ToList();
+                    return list;
+                })
+                .ToList();
+
+            foreach (var enumerable in result)
+            {
+                Console.WriteLine(string.Join(", ", enumerable));
+            }
+        }
+    }
+}
