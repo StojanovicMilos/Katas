93cc0c511429f27b46a2c35b39cc9ba6f08e6f2a
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-06-23 15:26:23 +0200

    solve palindrome number

---
+++ LeetCodeChallenges/PalindromeNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿// https://leetcode.com/problems/palindrome-number
+Console.WriteLine(IsPalindrome(55155));
+
+bool IsPalindrome(int x) {
+    if (x < 0)
+    {
+        return false;
+    }
+
+    if (x < 10)
+    {
+        return true;
+    }
+
+    string s = x.ToString();
+
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        if (s[i] != s[s.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
