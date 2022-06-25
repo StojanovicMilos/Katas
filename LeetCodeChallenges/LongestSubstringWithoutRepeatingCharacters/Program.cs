65f36a9d17711d69c210aef8adbed9b155b47acc
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-06-25 08:22:29 +0200

    solve longest substring without repeating

---
+++ LeetCodeChallenges/LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/longest-substring-without-repeating-characters
+Console.WriteLine("Hello, World!");
+var solution = LengthOfLongestSubstring("umvejcuka");
+Console.WriteLine(solution);
+
+int LengthOfLongestSubstring(string s)
+{
+    if(s == null)
+    {
+        return 0;
+    }
+
+    var unique = new List<char>();
+    int max = 0;
+    for (int right = 0; right < s.Length; right++)
+    {
+        if (!unique.Contains(s[right]))
+        {
+            unique.Add(s[right]);
+            max = Math.Max(unique.Count, max);
+        }
+        else
+        {
+            unique.RemoveRange(0, Convert.ToInt32(unique.IndexOf(s[right])) + 1);
+            unique.Add(s[right]);
+        }
+    }
+    return max;
+}
