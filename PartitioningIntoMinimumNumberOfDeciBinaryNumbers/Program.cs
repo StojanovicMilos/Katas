a520f271e38b8047b3e70bb5589e7ddf91a5224c
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-06-27 16:16:24 +0200

    solve partitioning into minimum number of deci binary numbers

---
+++ PartitioningIntoMinimumNumberOfDeciBinaryNumbers/Program.cs
@@ -0,0 +1,4 @@
+﻿// https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/submissions/
+Console.WriteLine("Hello, World!");
+
+int MinPartitions(string n) => string.IsNullOrEmpty(n) ? 0 : n.ToCharArray().Select(c => int.Parse(c.ToString())).Max();
