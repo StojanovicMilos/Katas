a0d0adc339fcdf28a044d48e72281a87bddb4e90
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-06-30 18:39:29 +0200

    solve 462. Minimum Moves to Equal Array Elements II

---
+++ LeetCodeChallenges/MinimumMovesToEqualArrayElementsII/Program.cs
@@ -0,0 +1,20 @@
+﻿// https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii
+Console.WriteLine(MinMoves2(new[] { 1, 0, 0, 8, 6 }));
+
+int MinMoves2(int[] nums)
+{
+    var median = Median(nums);
+    var sum = 0;
+    foreach (var number in nums)
+    {
+        sum += Math.Abs(number - median);
+    }
+    return sum;
+}
+
+int Median(int[] numbers)
+{
+    var ys = numbers.OrderBy(x => x).ToList();
+    double mid = (ys.Count - 1) / 2.0;
+    return (ys[(int)(mid)] + ys[(int)(mid + 0.5)]) / 2;
+}
