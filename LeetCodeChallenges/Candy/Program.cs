d3dca66ecc8a2c9f8578ad6e755b2fd96e1f7b96
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-07-04 17:37:15 +0200

    solve 135. Candy

---
+++ LeetCodeChallenges/Candy/Program.cs
@@ -0,0 +1,25 @@
+﻿// https://leetcode.com/problems/candy
+Console.WriteLine("Hello, World!");
+
+int Candy(int[] ratings)
+{
+    int[] res = Enumerable.Repeat<int>(1, ratings.Length).ToArray();
+
+    for (int i = 1; i < ratings.Length; i++)
+    {
+        if (ratings[i] > ratings[i - 1])
+        {
+            res[i] = res[i - 1] + 1;
+        }
+    }
+
+    for (int i = ratings.Length - 2; i >= 0; i--)
+    {
+        if (ratings[i] > ratings[i + 1])
+        {
+            res[i] = Math.Max(res[i], res[i + 1] + 1);
+        }
+    }
+
+    return res.Sum();
+}
