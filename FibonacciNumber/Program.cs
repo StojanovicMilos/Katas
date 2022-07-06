04f105508879e0f4c420471e934e9e190b73b285
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-07-06 17:12:34 +0200

    solve 509. Fibonacci Number

---
+++ FibonacciNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// https://leetcode.com/problems/fibonacci-number/
+Console.WriteLine("Hello, World!");
+
+int Fib(int n)
+{
+    if (n == 0 || n == 1) return n;
+    var prev = 0;
+    var curr = 1;
+    var next = 1;
+    for (int i = 2; i < n + 1; i++)
+    {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return next;
+}
