6f820e88dfafbd44b42018dcdd4a063ae8dbfd0b
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-09-19 21:41:26 +0200

    Solve 215. Kth Largest Element in an Array

---
+++ FreeCodeCamp/KthLargestElementInArray/Library.cs
@@ -0,0 +1,32 @@
+﻿namespace KthLargestElementInArray
+{
+    public static class Library
+    {
+        public static int FindLargestElementV1(int[] array, int k)
+        {
+            var item = array.OrderByDescending(element => element).Skip(k - 1).FirstOrDefault();
+            return item == default ? throw new InvalidOperationException() : item;
+        }
+
+        public static int FindLargestElementV2(int[] array, int k)
+        {
+            if(k > array.Length || k == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            PriorityQueue<int, int> queue = new PriorityQueue<int, int>(array.Length);
+            foreach (var item in array)
+            {
+                queue.Enqueue(item, -item);
+            }
+
+            for (int i = 0; i < k-1; i++)
+            {
+                _ = queue.Dequeue();
+            }
+
+            return queue.Dequeue();
+        }
+    }
+}
---
+++ FreeCodeCamp/KthLargestElementInArrayTests/LibraryTests.cs
@@ -0,0 +1,23 @@
+﻿//215. Kth Largest Element in an Array https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+using KthLargestElementInArray;
+using Xunit;
+
+namespace KthLargestElementInArrayTests
+{
+    public class LibraryTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1 }, 1, 1)]
+        [InlineData(new int[] { 1, 2 }, 1, 2)]
+        [InlineData(new int[] { 1, 2 }, 2, 1)]
+        [InlineData(new int[] { 3, 2, 1, 5, 6, 4 }, 2, 5)]
+        [InlineData(new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4, 4)]
+        public void AddWorksCorrectly(int[] array, int k, int expected)
+        {
+            var result = Library.FindLargestElementV2(array, k);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
