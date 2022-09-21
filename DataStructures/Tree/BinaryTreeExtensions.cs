8ee3ea11233f1004f026ae6e32580880a6c52026
Milos Stojanovic <milos-stojanovic@hotmail.com>
2022-09-21 18:51:31 +0200

    implement tree and it's searches

---
+++ DataStructures/Tree/BinaryTreeExtensions.cs
@@ -0,0 +1,100 @@
+﻿namespace Tree
+{
+    public static class BinaryTreeExtensions
+    {
+        public static List<int> DFSPreorder(this BinaryTree node)
+        {
+            List<int> result = new List<int>();
+            if (node == null)
+            {
+                return result;
+            }
+
+            result.Add(node.Value);
+            if (node.Left != null)
+            {
+                result.AddRange(node.Left.DFSPreorder());
+            }
+
+            if (node.Right != null)
+            {
+                result.AddRange(node.Right.DFSPreorder());
+            }
+
+            return result;
+        }
+
+        public static List<int> DFSPostOrder(this BinaryTree node)
+        {
+            List<int> result = new List<int>();
+            if (node == null)
+            {
+                return result;
+            }
+
+            if (node.Left != null)
+            {
+                result.AddRange(node.Left.DFSPostOrder());
+            }
+
+            if (node.Right != null)
+            {
+                result.AddRange(node.Right.DFSPostOrder());
+            }
+
+            result.Add(node.Value);
+            return result;
+        }
+
+        public static List<int> DFSInorder(this BinaryTree node)
+        {
+            List<int> result = new List<int>();
+            if (node == null)
+            {
+                return result;
+            }
+
+            if (node.Left != null)
+            {
+                result.AddRange(node.Left.DFSInorder());
+            }
+
+            result.Add(node.Value);
+
+            if (node.Right != null)
+            {
+                result.AddRange(node.Right.DFSInorder());
+            }
+
+            return result;
+        }
+
+        public static List<int> BFS(this BinaryTree node)
+        {
+            List<int> result = new List<int>();
+            if (node == null)
+            {
+                return result;
+            }
+
+            Queue<BinaryTree> queue = new Queue<BinaryTree>();
+            queue.Enqueue(node);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+                result.Add(current.Value);
+                if (current.Left != null)
+                {
+                    queue.Enqueue(current.Left);
+                }
+                if (current.Right != null)
+                {
+                    queue.Enqueue(current.Right);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DataStructures/Tree/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tree;
+
+BinaryTree root = new BinaryTree
+{
+    Value = 1,
+    Left = new BinaryTree
+    {
+        Value = 2,
+        Left = new BinaryTree
+        {
+            Value = 4,
+            Left = new BinaryTree { Value = 8 },
+            Right = new BinaryTree { Value = 9 }
+        },
+        Right = new BinaryTree
+        {
+            Value = 5,
+            Left = new BinaryTree { Value = 10 },
+            Right = new BinaryTree { Value = 11 }
+        }
+    },
+    Right = new BinaryTree
+    {
+        Value = 3,
+        Left = new BinaryTree
+        {
+            Value = 6,
+            Left = new BinaryTree { Value = 12 },
+            Right = new BinaryTree { Value = 13 }
+        },
+        Right = new BinaryTree
+        {
+            Value = 7,
+            Left = new BinaryTree { Value = 14 },
+            Right = new BinaryTree { Value = 15 }
+        }
+    }
+};
+
+Console.WriteLine($"dfspreorder: {string.Join(", ", root.DFSPreorder())}");
+Console.WriteLine($"dfspostorder: {string.Join(", ", root.DFSPostOrder())}");
+Console.WriteLine($"dfsinorder: {string.Join(", ", root.DFSInorder())}");
+Console.WriteLine($"bfs: {string.Join(", ", root.BFS())}");
