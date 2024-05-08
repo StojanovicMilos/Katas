1f48fc2c477b2961aad37f9d727d8613a67d42d7
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-05-08 18:39:30 +0200

    Solve 506. Relative Ranks

---
+++ LeetCodeChallenges/RelativeRanks/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RelativeRanks;
+
+Console.WriteLine("Hello, World!");
+
+int[] score = [5, 4, 3, 2, 1];
+var solution = Solution.FindRelativeRanks(score);
+foreach (var rank in solution)
+{
+    Console.WriteLine(rank);
+}
---
+++ LeetCodeChallenges/RelativeRanks/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace RelativeRanks
+{
+    //https://leetcode.com/problems/relative-ranks/description/?envType=daily-question&envId=2024-05-08
+    public static class Solution
+    {
+        public static string[] FindRelativeRanks(int[] score)
+        {
+            return score
+                .Select((value, index) => new
+                {
+                    Index = index,
+                    Value = value
+                })
+                .OrderByDescending(x => x.Value)
+                .Select((value, index) => new
+                {
+                    Index = value.Index,
+                    Value = Map(index)
+                })
+                .OrderBy(x => x.Index)
+                .Select(x => x.Value)
+                .ToArray();
+        }
+
+        private static string Map(int index)
+        => index switch
+        {
+            0 => "Gold Medal",
+            1 => "Silver Medal",
+            2 => "Bronze Medal",
+            _ => (index+1).ToString()
+        };
+    }
+}
