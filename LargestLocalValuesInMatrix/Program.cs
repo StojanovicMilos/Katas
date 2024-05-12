d0903dda0153692120d56832ce0cbc8c1d1598aa
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-05-12 09:17:20 +0200

    Solve 2373. Largest Local Values in a Matrix

---
+++ LargestLocalValuesInMatrix/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LargestLocalValuesInMatrix;
+
+Console.WriteLine("Hello, World!");
+
+int[][] grid = [[9, 9, 8, 1], [5, 6, 2, 6], [8, 2, 6, 4], [6, 2, 2, 2]];
+Solution.LargestLocal(grid);
---
+++ LargestLocalValuesInMatrix/Solution.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestLocalValuesInMatrix;
+//https://leetcode.com/problems/largest-local-values-in-a-matrix/?envType=daily-question&envId=2024-05-12
+internal static class Solution
+{
+    public static int[][] LargestLocal(int[][] grid)
+    {
+        int[][] solution = new int[grid.Length - 2][];
+
+        for (int i = 0; i < grid.Length - 2; i++)
+        {
+            solution[i] = new int[grid[i].Length - 2];
+        }
+
+        for (int i = 1; i < grid.Length - 1; i++)
+        {
+            for (int j = 1; j < grid[i].Length - 1; j++)
+            {
+                solution[i - 1][j - 1] = GetMax(grid, i, j);
+            }
+        }
+
+        return solution;
+    }
+
+    private static int GetMax(int[][] grid, int i, int j)
+    {
+        int[] items = [
+            GetTopLeft(grid, i, j), GetTop(grid, i, j), GetTopRight(grid, i, j),
+            GetLeft(grid, i, j), GetCenter(grid, i, j), GetRight(grid, i, j),
+            GetBottomLeft(grid, i, j), GetBottom(grid, i, j), GetBottomRight(grid, i, j)
+            ];
+
+        return items.Max();
+    }
+
+    private static int GetTopLeft(int[][] grid, int i, int j)
+    {
+        return grid[i - 1][j - 1];
+    }
+
+    private static int GetTop(int[][] grid, int i, int j)
+    {
+        return grid[i - 1][j];
+    }
+
+    private static int GetTopRight(int[][] grid, int i, int j)
+    {
+        return grid[i - 1][j + 1];
+    }
+
+    private static int GetLeft(int[][] grid, int i, int j)
+    {
+        return grid[i][j - 1];
+    }
+
+    private static int GetCenter(int[][] grid, int i, int j)
+    {
+        return grid[i][j];
+    }
+
+    private static int GetRight(int[][] grid, int i, int j)
+    {
+        return grid[i][j + 1];
+    }
+
+    private static int GetBottomLeft(int[][] grid, int i, int j)
+    {
+        return grid[i + 1][j - 1];
+    }
+
+    private static int GetBottom(int[][] grid, int i, int j)
+    {
+        return grid[i + 1][j];
+    }
+
+    private static int GetBottomRight(int[][] grid, int i, int j)
+    {
+        return grid[i + 1][j + 1];
+    }
+}
