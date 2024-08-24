cb524650f399889a7f69ead91e51076f43e400f3
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-08-24 09:46:12 +0200

    fix the build #oops

--- CrackingTheCodingInterview/DispatchSystem/Director.cs
+++ CrackingTheCodingInterview/DispatchSystem/Director.cs
@@ -4,5 +4,5 @@ internal class Director : BaseEmployee
     internal override int Level => 2;
 
     //question for the interviewer - what if director cannot handle the call? Is that possible?
-    internal override void EscalateCall(IDispatchSystem dispatchSystem) => throw new InvalidOperationException("Director has nobody to escalate the call to");
+    internal override void EscalateCall(IEscalateCallSystem escalateCallSystem) => throw new InvalidOperationException("Director has nobody to escalate the call to");
 }
