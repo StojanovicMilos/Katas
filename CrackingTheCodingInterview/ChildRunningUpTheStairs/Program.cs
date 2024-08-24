c2ad238b8a4a5afd9db3e41cffa377d27dc12df7
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-08-24 09:46:45 +0200

    9.1 solve child running up the stairs

---
+++ CrackingTheCodingInterview/ChildRunningUpTheStairs/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine(Child.RunUpStaircase(10));
+
+static class Child
+{
+    static readonly int[] steps = [1, 2, 3];
+    internal static int RunUpStaircase(int remainingSteps)
+        => remainingSteps == 0
+            ? 1
+            : steps.Where(step => step <= remainingSteps)
+                    .Sum(step => RunUpStaircase(remainingSteps - step));
+}
