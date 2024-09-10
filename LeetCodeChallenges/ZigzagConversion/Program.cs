106203a02d61b61d92362d74586f7877a3a4d1ee
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-09-10 17:01:43 +0200

    solve 6. Zigzag Conversion

---
+++ LeetCodeChallenges/ZigzagConversion/Program.cs
@@ -0,0 +1,43 @@
+﻿//https://leetcode.com/problems/zigzag-conversion/
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1 || s.Length <= numRows)
+        {
+            return s;
+        }
+
+        var rows = new List<StringBuilder>();
+        for (var i = 0; i < numRows; i++)
+        {
+            rows.Add(new StringBuilder());
+        }
+
+        var currentRow = 0;
+        var goingDown = false;
+
+        foreach (var c in s)
+        {
+            rows[currentRow].Append(c);
+            if (currentRow == 0 || currentRow % (numRows - 1) == 0)
+            {
+                goingDown = !goingDown;
+            }
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        var result = new StringBuilder();
+        foreach (var row in rows)
+        {
+            result.Append(row);
+        }
+
+        return result.ToString();
+    }
+}
