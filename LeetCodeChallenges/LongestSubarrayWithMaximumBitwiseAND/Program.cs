96fc30db1aeddf8566626e40ccf8400287f06fcd
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-09-14 19:11:04 +0200

    solve 2419. Longest Subarray With Maximum Bitwise AND

---
+++ LeetCodeChallenges/LongestSubarrayWithMaximumBitwiseAND/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int LongestSubarray(int[] nums)
+    {
+        var result = 0;
+        if (nums == null || nums.Length == 0)
+        {
+            return result;
+        }
+
+        var max = nums.Max();
+        var currentLength = 0;
+        foreach (int num in nums)
+        {
+            if (num == max)
+            {
+                currentLength++;
+                if (currentLength > result)
+                {
+                    result = currentLength;
+                }
+            }
+            else
+            {
+                currentLength = 0;
+            }
+        }
+
+        return result;
+    }
+}
