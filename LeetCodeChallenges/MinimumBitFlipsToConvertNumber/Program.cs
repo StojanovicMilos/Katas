ac7855f8190e66e806f1edc66e07813c2b32b0b6
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-09-14 19:35:56 +0200

    solve 2220. Minimum Bit Flips to Convert Number

---
+++ LeetCodeChallenges/MinimumBitFlipsToConvertNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿// https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int MinBitFlips(int start, int goal)
+    {
+        var number = start ^ goal;
+        var count = 0;
+        while (number > 0)
+        {
+            count += number & 1;
+            number >>= 1;
+        }
+        return count;
+    }
+}
