0f870d6e10a8f088cd2debd259fb15528255bb1e
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-09-14 19:17:56 +0200

    solve 1310. XOR Queries of a Subarray

---
+++ XorQueriesOfSubarray/Program.cs
@@ -0,0 +1,22 @@
+﻿// https://leetcode.com/problems/xor-queries-of-a-subarray/description/
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int[] XorQueries(int[] arr, int[][] queries)
+    {
+        var result = new List<int>();
+        foreach (var query in queries)
+        {
+            var xorResult = 0;
+            for (var i = query[0]; i <= query[1]; i++)
+            {
+                xorResult ^= arr[i];
+            }
+
+            result.Add(xorResult);
+        }
+
+        return result.ToArray();
+    }
+}
