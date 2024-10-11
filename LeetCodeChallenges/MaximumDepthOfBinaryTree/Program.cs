fa4dfc49960c4ffb12630d87ba1a25ca61719500
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-10-11 17:09:45 +0200

    solve 104. Maximum Depth of Binary Tree - missing code

--- LeetCodeChallenges/MaximumDepthOfBinaryTree/Program.cs
+++ LeetCodeChallenges/MaximumDepthOfBinaryTree/Program.cs
@@ -15,3 +15,16 @@ public class Solution
         return Math.Max(MaxDepth(node.left, depth + 1), MaxDepth(node.right, depth + 1));
     }
 }
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
