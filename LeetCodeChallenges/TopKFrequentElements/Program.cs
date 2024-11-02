632bc5a34143be37888ddd35babf8d71e0a17cd2
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-11-02 13:15:49 +0100

    solve 347. Top K Frequent Elements

---
+++ LeetCodeChallenges/TopKFrequentElements/Program.cs
@@ -0,0 +1,14 @@
+﻿// https://leetcode.com/problems/top-k-frequent-elements/description/
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        return nums.GroupBy(n => n)
+                .OrderByDescending(g => g.Count())
+                .Take(k)
+                .Select(g => g.Key)
+                .ToArray();
+    }
+}
