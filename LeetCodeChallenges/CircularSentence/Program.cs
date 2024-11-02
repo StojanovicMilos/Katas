4f4ff3e34a9ef3b996ea8730c5fa728eaa19707c
Milos Stojanovic <milos-stojanovic@hotmail.com>
2024-11-02 12:54:49 +0100

    solve 2490. Circular Sentence

---
+++ LeetCodeChallenges/CircularSentence/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Solution
+{
+    public bool IsCircularSentence(string sentence)
+    {
+        if (sentence == null)
+        {
+            return false;
+        }
+
+        if (sentence.Length <= 1)
+        {
+            return true;
+        }
+
+        if (sentence[0] != sentence[sentence.Length - 1])
+        {
+            return false;
+        }
+
+        for (var i = 1; i < sentence.Length - 1; i++)
+        {
+            if ((sentence[i] == ' ') && (sentence[i - 1] != sentence[i + 1]))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
